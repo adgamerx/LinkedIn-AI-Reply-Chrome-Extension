45ee458d4a159803cab456287e2488fd12082606
adgamerx <amank.u.m100@gmail.com>
2024-10-27 01:39:29 +0530

    ✅ Refactor tailwind configuration and entrypoints

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import { useState } from "react";
+import reactLogo from "@/assets/react.svg";
+import wxtLogo from "/wxt.svg";
+import "./style.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-32 w-64 flex justify-center items-center bg-sky-100">
+        <div className="flex flex-col items-center">
+        <p className="text-xl font-semibold ">Linkedin AI Extension </p>
+        <span className="text-l font-semibold text-blue-500">v1.0</span>
+        <span className="text-l font-semibold ">By: <a href="https://github.com/adgamerx/">Aman Sharma</a></span>
+        </div>
       </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
     </>
   );
 }
